e9ae07cccb067f22b4784467c9fdb0905344a879
paul martin <mussym@live.com>
2020-07-03 00:00:09 -0500

    stuffing about with ant design

--- like_button.js
+++ like_button.js
@@ -1,26 +1,55 @@
 'use strict';
-const React = require('react')
-const ReactDOM = require('react-dom')
+const React = require('react');
+const ReactDOM = require('react-dom');
+const {
+    Tabs: ant_tabs,
+    DatePicker: ant_date_picker,
+    TimePicker: ant_time_picker,
+    Button: ant_button,
+    primary,
+    dashed,
+    defaultActiveKey
+} = require('antd');
 
 
+function callback(key){
+    console.log(key);
+}
+
+const {tab_pane} = ant_tabs;
 const e = React.createElement;
+
+const tabPane = e(ant_tabs, {tab: 'Tab 1', key: '1'}, 'blah blah' );
+const tabPaneTwo = e(ant_tabs, {tab: 'Tab 2', key: '2'}, 'contents baby' );
+const tab = e(ant_tabs, {defaultActiveKey: 1}, [tabPane, tabPaneTwo]);
+const timePicker = e(ant_time_picker, { },  );
+const datePicker = e(ant_date_picker, { },  );
+const responseHeada = e('h1', {}, 'the hell');
+const responseBodda = e('h2', { style: { color: "red" }}, 'is goan on');
+const buttonResponse = e('div', {}, [responseHeada, responseBodda,tab, datePicker, timePicker]);
+
 
 class LikeButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = { liked: false };
-    }
 
-    render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
-        return e(
+        this.button = e(
             'button',
             { onClick: () => this.setState({ liked: true }) },
             'Like'
         );
+    }
+
+    
+
+    render() {
+        if (this.state.liked) {
+            //return 'You liked this.';
+            return buttonResponse;
+        }
+
+        return this.button;
     }
 }
 
