9c1077d4d79be95716aef57cee13165bbca9f55d
paul martin <mussym@live.com>
2020-07-04 23:15:54 -0500

    trying out a splitter

--- src/shelf.jsx
+++ src/shelf.jsx
@@ -8,18 +8,50 @@ const {
     Footer,
     Row,
     Col,
-    Divider
+    Divider,
+    flex
 } = require('antd');
 
+const Split = require('react-split');
+
+
 const e = React.createElement;
+
+
+const splitPane = (
+    <Split  sizes={[25, 75]}
+            minSize={100}
+            expandToMin={false}
+            gutterSize={10}
+            gutterAlign="center"
+            snapOffset={30}
+            dragInterval={1}
+            direction="horizontal"
+            cursor="col-resize"
+    >
+      <Row>
+        <Col><button>shelf</button></Col>
+        <Col flex={3}>
+          <Row>
+            <Col><button>subject</button></Col>
+          </Row>
+          <Row>
+            <Col><button>publication</button></Col>
+          </Row>
+        </Col>
+      </Row>
+      <div>hello thar</div>
+    </Split>
+);
+
+
+
 class ShelfContainer extends React.Component {
 
     render() {
-        return <Row>
-                 <Col><button>shelf</button></Col>
-                 <Col><button>subject</button></Col>
-                 <Col><button>publications</button></Col>
-               </Row>;
+        return (
+            splitPane
+        );
     }
 }
 
