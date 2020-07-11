0f5d4753fe5143a0eb237876802fa3fe093f3aae
paul martin <mussym@live.com>
2020-07-10 21:09:04 -0500

    fixed the issue with require splitters of all sorts

--- src/shelf.jsx
+++ src/shelf.jsx
@@ -14,8 +14,9 @@ const {
 } = require('antd');
 const TabPane = Tabs.TabPane;
 
-const Split = require('react-split');
 
+const SplitterLayout = require('react-splitter-layout').default;
+const SplitPane = require('react-split-pane').default;
 
 const e = React.createElement;
 
@@ -23,16 +24,6 @@ const e = React.createElement;
 const splitPane = (
     <Tabs>
       <TabPane tab="Shelf" key="shelf">
-        <Split  sizes={[25, 75]}
-            minSize={100}
-            expandToMin={false}
-            gutterSize={10}
-            gutterAlign="center"
-            snapOffset={30}
-            dragInterval={1}
-            direction="horizontal"
-            cursor="col-resize"
-            >
       <div className="split" id="shelf_pane">
       <Row>
         <Col><button>shelf</button></Col>
@@ -49,10 +40,44 @@ const splitPane = (
       <div className="split" id="subject_pane">
         <div>hello thar</div>
       </div>
-    </Split>
       </TabPane>
       <TabPane tab="Home" key="home">
         <div><h1>Home Screen</h1></div>
+        {/* <SplitPane split="vertical"> */}
+        {/*   <h1>shite</h1> */}
+        {/*   <h2>shoote</h2> */}
+        {/* </SplitPane> */}
+
+        <SplitterLayout>
+          <div>
+            <Row>
+              <Col><button>shelf</button></Col>
+              <Col flex={3}>
+                <Row>
+                  <Col><button>subject</button></Col>
+                </Row>
+                <Row>
+                  <Col><button>publication</button></Col>
+                </Row>
+              </Col>
+            </Row>
+
+          </div>
+          <div>
+            <Row>
+              <Col><button>shelf</button></Col>
+              <Col flex={3}>
+                <Row>
+                  <Col><button>subject</button></Col>
+                </Row>
+                <Row>
+                  <Col><button>publication</button></Col>
+                </Row>
+              </Col>
+            </Row>
+
+          </div>
+        </SplitterLayout>
       </TabPane>
     </Tabs>
 );
@@ -70,4 +95,5 @@ class ShelfContainer extends React.Component {
 
 
 const domContainer = document.querySelector('#main_container');
+
 ReactDOM.render(e(ShelfContainer), domContainer);
