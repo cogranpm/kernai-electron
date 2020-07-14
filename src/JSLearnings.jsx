7f26090db964a5481634be3bf3040bceb304d8df
paul martin <mussym@live.com>
2020-07-13 23:14:35 -0500

    adding javascript stuff

--- src/JSLearnings.jsx
+++ src/JSLearnings.jsx
@@ -1,10 +1,14 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const {
+    Collapse
+} = require('antd');
+const {Panel} = Collapse;
 const e = React.createElement;
 
-class JSLearnings extends React.Component {
 
-    
+class Encapsulation extends React.Component {
+
     constructor(props){
         super(props);
     }
@@ -33,8 +37,22 @@ class JSLearnings extends React.Component {
                 return "Hello " + fred;
             }
         }
-        })();
+        })();        
         `
+
+        const function_returns_object = `
+        function createCircle(radius){
+            return {
+                area: function() {
+                    return radius * radius * Math.PI;
+                }
+            }
+        };
+        let circle = createCircle(3)
+        circle.area();
+        circle.radius;
+        `
+
         return (
             <div>
             <div>Javascript</div>
@@ -63,7 +81,101 @@ class JSLearnings extends React.Component {
                 eg:
                 {iife_example_object}
             </p>
+            <p>
+                The other variations are to create a function that returns an anonymous object:
+                {function_returns_object}
+            </p>
             </div>
+        );
+    }
+}
+
+class JSLearnings extends React.Component {
+
+    
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        const object_init = `
+            var obj = { prop1 : value1,
+                        prop2 : value2
+            }
+            //may need to wrap in () if appears where statement is expected
+        `
+
+        const constructor_function = `
+            function Car(make, model, year){
+                this.make = make;
+                this.model = model;
+                this.year = year
+            }
+            var myCar = new Car("Eagle", "Talon", 1993);
+            //add a property on the fly
+            myCar.color = "Black";
+        `
+
+        const object_create = `
+            var Animal = {
+                type: 'Invertebrates',
+                displayType: function(){
+                    console.log(this.type);
+                }
+            };
+
+            //pass in the prototype of Animal
+            var animal1 = Object.create(Animal);
+            animal1.displayType();
+
+            var fish = Object.create(Animal);
+            fish.type = "Fishes";
+            fish.displayType();
+        `
+
+
+        return (
+            <Collapse defaultActiveKey={['1']}>
+                <Panel header="Encapsulation" key="1">
+                    <Encapsulation></Encapsulation>
+                </Panel>
+                <Panel header="Grammar and Types" key="2">
+                    <div>
+                        <h3>hoisting</h3>
+                        <p>variables declared with var regardless of where declared are moved to top of function</p>
+                        <h3>data types</h3>
+                        <ol>
+                            <li>Boolean</li>
+                            <li>null</li>
+                            <li>undefined</li>
+                            <li>Number</li>
+                            <li>BigInt</li>
+                            <li>String</li>
+                            <li>Symbol</li>
+                            <li>Object</li>
+                        </ol>
+                        <h3>Literals</h3>
+                        <ul>
+                            <li><h4>Array:</h4>let coffees = ['French Roast', 'Colombian', 'Kona']</li>
+                            <li><h4>Object:</h4></li>
+                            <li><h4>Enhanced Object:</h4></li>
+                            <li><h4>Regex:</h4></li>
+                            <li><h4>String:</h4></li>
+                        </ul>
+                        <h3>Object Initializer</h3>
+                        <p>ya know, these are really dictionaries</p>
+                        <p>{object_init}</p>
+                        <h3>Constructor Functions</h3>
+                        <p>this is familiar from my osi days</p>
+                        <p>{constructor_function}</p>
+                        <h3>Object.create method</h3>
+                        <p>allows to pass a prototype variable
+                            which defines the built in stuff
+                        </p>
+                        <p>{object_create}</p>
+                    </div>
+                </Panel>
+            </Collapse>
         );
     }
 }
